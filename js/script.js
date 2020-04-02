1abcecd222c1c2b9d33f4f41c21dca41171693e9
Arjun <arj.kashyap123@gmail.com>
2020-04-03 02:16:08 +0530

    Snake movements and size increase, fixed size increase bug

--- js/script.js
+++ js/script.js
@@ -1,24 +1,53 @@
-const box = 30;         // Unit for measure
+// Unit for measure
+const box = 30;
+
+// Assets
+const ground = new Image();
+ground.src = "../img/background.jpg"
+
 const foodImg = new Image();
 foodImg.src = "../img/food.png";
 
 let score = 0;
 
-let mouse = {
+let food = {
     x: Math.floor(Math.random() * 17 + 1) * box,
     y: Math.floor(Math.random() * 15 + 3) * box
 };
 
 let snake = [];         // Snake Array 
 snake[0] = {x: 9*box, y: 10*box};    // Snake head
-snake[1] = {x: 8*box, y: 10*box};
+//snake[1] = {x: 8*box, y: 10*box};
+console.log(snake)
+
+// Current snake direction
+let d;
+
+document.addEventListener("keydown", direction);
+
+// Function Changes the direction of the snake movement
+function direction(event){
+    if(event.keyCode == 37 && d != "RIGHT"){
+        d = "LEFT";
+    }else if( event.keyCode == 38 && d != "DOWN"){
+        d = "UP";
+    }else if( event.keyCode == 39 && d != "LEFT"){
+        d = "RIGHT";
+    }else if( event.keyCode == 40 && d != "UP"){
+        d = "DOWN";
+    }
+}
 
 function draw(){
+    
+    if(snake.length > 1)
+        console.log(snake)
+
     const canvas = document.getElementById('game-area');
     const ctx = canvas.getContext('2d');
 
     if( ctx ){
-
+        ctx.drawImage(ground,0,0);
         snake.forEach( (s, index) => {
     
             ctx.fillStyle = ( index == 0 ) ? 'green' : 'white';
@@ -27,21 +56,55 @@ function draw(){
             ctx.strokeRect(s.x, s.y, box, box);
         } )
 
-        // Draw mouse at random places
-        ctx.drawImage(foodImg, mouse.x, mouse.y);
+        
+
+        // Draw food at random places
+        ctx.drawImage(foodImg, food.x, food.y);
 
         // Score
         ctx.fillStyle = 'white';
         ctx.font = '45px Changa One';
-        console.log(2*box)
+       
         ctx.fillText(score, 2*box, box);
-        console.log(box)
+        
+
+        // Old snake head position
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+        
+        // Update Position
+        if( d == "RIGHT" ) snakeX += box;
+        if( d == "LEFT" ) snakeX -= box;
+        if( d == "UP" ) snakeY -= box;
+        if( d == "DOWN" ) snakeY += box;
+
+
+        // If snake eats the food
+        if( snake[0].x == food.x && snake[0].y == food.y ){
+            score ++; 
+            food = {
+                x: Math.floor(Math.random() * 17 + 1) * box,
+                y: Math.floor(Math.random() * 15 + 3) * box
+            };
+        }
+        else{
+            snake.pop();
+        }
+        
+
+        let newHead = {
+            x: snakeX,
+            y: snakeY
+        }
+
+        snake.unshift(newHead);
+
     }
     else{
         document.write('Some Error Occured\n');
     }
 }
 
-//let game = setInterval(draw, 100);
+let game = setInterval(draw, 100);
 
 
